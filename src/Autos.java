98eb84ef29fceb87e500c50bf37077f34b37b186
Dustin <dustin04ms@gmail.com>
2023-11-21 08:49:11 -0500

    Initial commit

--- src/Autos.java
+++ src/Autos.java
@@ -1,3 +1,46 @@
 public class Autos {
+    //ATRIBUTOS
+    public String marcas;
+    public String color;
+    public float precio;
+    public int anio;
 
+    //CONSTRUCTOR
+    public Autos(String nuevoAuto){
+        marcas=nuevoAuto;
+    }
+
+    //METODOS
+    public String getMarcas() {
+        return marcas;
+    }
+    public String getColor(){
+        return color;
+    }
+    public float getPrecio(){
+        return precio;
+    }
+    public int getAnio(){
+        return anio;
+    }
+
+    public void setMarcas(String marcas) {
+        this.marcas = marcas;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public void imprimirDatos(){
+        System.out.println(getAnio()+getPrecio()+getColor()+getMarcas());
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Autos auto1=new Autos("Pichirilo");
+        System.out.println(auto1.marcas);
     }
+
 }
