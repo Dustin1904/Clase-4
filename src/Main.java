d6087a33e135ab7c52e74a2a91788fac56ea2f6f
Alejandro <alejandro27campoverde@gmail.com>
2023-11-21 09:35:40 -0500

    XD

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Autos auto1=new Autos("Pichirilo");
         System.out.println(auto1.marcas);
+        //auto1.setColor("Rojo");
+        auto1.setAnio(2022);
+        auto1.imprimirDatos();
+        System.out.println("Suma de años");
+        System.out.println(auto1.getAnio());
+
+       // auto2.setAnio(2023);
+       // auto1.setMarcas("Toyota");
+       // auto2.setMarcas("Chevrolet");
+
+        System.out.println("Valores nuevos");
+        auto1.imprimirDatos();
+        //auto2.imprimirDatos();
+
+
+        String marca;
+        Scanner dato=new Scanner(System.in);
+        System.out.println("Ingrese la marca");
+        marca=dato.nextLine();
+        //System.out.println(marca);
+        auto1.setMarcas(marca);
+        auto1.imprimirDatos();
+
+
     }
 
 }
