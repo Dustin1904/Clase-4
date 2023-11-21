f24d45d1b382e5af8646df7b678e9233ffda2bc1
Dustin <dustin04ms@gmail.com>
2023-11-21 08:57:05 -0500

    2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         Autos auto1=new Autos("Pichirilo");
+        auto1.setAnio(2023);
+        auto1.setColor("rojo");
         System.out.println(auto1.marcas);
+        auto1.imprimirDatos();
     }
 
 }
